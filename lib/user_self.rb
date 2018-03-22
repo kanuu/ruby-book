63a1f6f44d045e541a346fa870922ceaccda1b12
kanuu <123kanuu@gmail.com>
2018-03-22 23:32:43 +0900

    add self name

--- lib/user_self.rb
+++ lib/user_self.rb
@@ -16,8 +16,29 @@ class UserSelf
   def my_name
     "My name is #{@name}"
   end
+
+  def rename_to_bob
+    name = 'Bob'
+  end
+
+  def rename_to_dave
+    @name = 'Dave'
+  end
+
+  def rename_to_carol
+    self.name = 'Carol'
+  end
 end
 p user = UserSelf.new('Alice')
 p user.hello
 p user.hi
 p user.my_name
+
+p user.rename_to_bob
+p user.name
+
+p user.rename_to_dave
+p user.name
+
+p user.rename_to_carol
+p user.name
